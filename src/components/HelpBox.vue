3b0f4dda1fded798993c66a728320fd325347848
Andrew Fallows <andrew.fallows@intervarsity.org>
2018-08-16 14:58:42 -0400

    update help text; add Avenir to main CSS

--- src/components/HelpBox.vue
+++ src/components/HelpBox.vue
@@ -5,17 +5,27 @@
       <span id="help-close">close</span>
     </a>
     <div id="help-body">
-      Here are some tips for using this tool
+      <h3>Instructions for Using the Logo Generator</h3>
 
       <h4>Filling out the form</h4>
       <ol>
-        <li>School Name should be the full name of your college or university, e.g. University of College</li>
-        <li>Short Name may be the school's initials or a similar shorthand, like UofC</li>
+        <li>School, area, or region name should be the full name or your college, university, area, or region, e.g. “University of College” or “Greater Los Angeles.”</li>
+        <li>Abbreviated name may use initials or similar shorthand, like “UofC”.</li>
+        <li>Use a second line ONLY if your full name is longer than the character limit, or if you’re creating a logo for a focused ministry such as “International Student Ministry / at University of College.”</li>
+        <li>If you change your mind about needing a second line, click “RESET” to reset the form. [*include this only if you can’t figure out how to get a button that removes the second line]</li>
       </ol>
+
       <h4>Using the logo files</h4>
       <ol>
-        <li>When you click "Download ZIP", you will receive a ZIP file with all the displayed logos in it. You do not need to use all of them, but they're all ready for you to use!</li>
-        <li>Inverted color logos are all white with a transparent background; they may look blank in some file browsers or image applications. Don't worry! The logo and text will be there, just put it over a dark background to see it.</li>
+        <li>When you click “Download ZIP,” you will receive a ZIP file with all the displayed logos in it. PNG files are for digital and small print use, SVG files are for large scale printing. You do not need to use all of the files, but they’re all ready for you to use!</li>
+        <li>Inverted color logos are all white with a transparent backgrounds. They may look blank in some file browsers or image applications. Don’t worry! The logo and text will be there, just put it over a dark background to see it.</li>
+      </ol>
+
+      <h4>Other info</h4>
+      <ol>
+        <li>Once you download your files, a copy of the files will be sent to the 2100 Design team, who will review all generated logos periodically. If there are any issues, someone will get in touch via the email you provide.</li>
+        <li>Remember to refer to the Brand Book for proper usage of the logo.</li>
+        <li>Questions? Contact xxxx [*Mark, do you think we need this? Whose email should we provide?]</li>
       </ol>
     </div>
   </div>
@@ -65,11 +75,11 @@ export default {
   position: absolute;
   top: 0;
   right: 0;
-  width: 500px;
-  height: 500px;
+  width: 80%;
+  min-height: 500px;
   background: white;
   color: #006680;
-  padding: 15px;
+  padding: 25px;
   -moz-box-shadow:    8px 8px 17px 2px rgba(00, 66, 80, 0.3);
   -webkit-box-shadow: 8px 8px 17px 2px rgba(00, 66, 80, 0.3);
   box-shadow:         8px 8px 17px 2px rgba(00, 66, 80, 0.3);
