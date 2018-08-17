f1bdda41d0c3d2eb5c7dd2855ccac041785e39db
Andrew Fallows <andrew.fallows@intervarsity.org>
2018-08-17 10:22:33 -0400

    fix typo

--- src/components/SquareLogoForm.vue
+++ src/components/SquareLogoForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h4>Verical Logo with Abbreviated Name</h4>
+    <h4>Vertical Logo with Abbreviated Name</h4>
     <div id="canvas-container">
       <canvas class='cnv-logo' id="cnv-logo-square" :width="this.canvasWidth" :height="this.canvasHeight"></canvas>
     </div>
--- src/components/SquareLogoInverse.vue
+++ src/components/SquareLogoInverse.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h4>Verical Logo with School Short Name</h4>
+    <h4>Vertical Logo with School Short Name</h4>
     <div id="canvas-container">
       <canvas class='cnv-logo' id="cnv-logo-square-inverse" :width="this.canvasWidth" :height="this.canvasHeight"></canvas>
     </div>
